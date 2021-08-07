1fb9d5d0708ebac322e72be57897dd44eeb65e6b
mitr900 <44660576+mitr900@users.noreply.github.com>
2021-08-07 17:09:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -27,6 +27,12 @@ if($arrJson['events'][0]['message']['text'] == "hi"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+ 
+}else if($arrJson['events'][0]['message']['text'] == "บ้า"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "พ่อง..มึงตายยย";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
